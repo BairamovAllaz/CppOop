c79cea09cecf42e48584722de6c50af48727553b
Bayramov Allaz <67458149+bayramovellez@users.noreply.github.com>
2021-11-24 20:20:24 +0300

    New

--- Distance/Task.cpp
+++ Distance/Task.cpp
@@ -1,40 +1,71 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-
 class M
 {
-    int x1, x2, y1, y2;
-    public:
-        M(int x1, int x2, int y1, int y2)
-        {
-            this->x1 = x1;
-            this->x2 = x2;
-            this->y1 = y1;
-            this->y2 = y2;
-        }
-        //METHODS
-        //method for power of number
-        int pow(int num)
-        {
-            return num * num;
-        }
-        int distance()
-        {
-            return sqrt(pow((x2 - x1) + pow(y2 - y1)));
-        }
-        ~M(){ 
-            cout << "I am desctructor: " << endl;
-        }
+    double x, y;
+public:
+    ///constructor
+    M(double x, double y)
+    {
+        this->x = x;
+        this->y = y;
+    }
+    ///get set methods
+    double get_x() const
+    {
+        return x;
+    }
+
+    double get_y() const
+    {
+        return y;
+    }
+
+    void set_x(double x)
+    {
+        this->x = x;
+    }
+
+    void get_y(double y)
+    {
+        this->y = y;
+    }
+
+    //METHODS
+    //method for power of number
+    double pow(double num)
+    {
+        return num * num;
+    }
+    ///method distance
+    double distance(M other)
+    {
+        return sqrt(pow((this->x - other.x)) + pow((this->y - other.y)));
+        // double x_distance = this->x - other.x;
+        // double y_distance = this->y - other.y;
+        // return sqrt(x_distance * x_distance + y_distance * y_distance);
+    }
+    ///descturctors
+    ~M()
+    {
+        cout << "I am desctructor: " << endl;
+    }
 };
+//global distance function
+double distance2(M a, M b)
+{
+    return sqrt(a.pow((a.get_x() - b.get_x())) + a.pow((a.get_y() - b.get_y())));
+    // double x_distance = a.get_x() - b.get_x();
+    // double y_distance = a.get_y() - b.get_y();
+    // return sqrt(x_distance * x_distance + y_distance * y_distance);
+}
 
 int main()
 {
-
-    cout << "Hello world: " << endl;
-    M n(2, 0, 2, 3);
-
-    cout << n.distance() << endl;
-
+    M n(2, 3);
+    M a(3, 4);
+    cout << "Value: " << n.distance(a) << endl;
+    cout << "Value 2: " << distance2(n, a) << endl;
     return 0;
 }
