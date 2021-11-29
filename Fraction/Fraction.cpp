685cba7b867ec2576b53f7dbc61142c0b88d0337
Bayramov Allaz <67458149+bayramovellez@users.noreply.github.com>
2021-11-29 22:20:47 +0300

    Work

--- Fraction/Fraction.cpp
+++ Fraction/Fraction.cpp
@@ -6,42 +6,34 @@ class Fraction
     int integer;
     int numerator;
     int denominator;
-
 public:
     //Encasilpation
-
     int get_integer() const
     {
         return integer;
     }
-
     int get_numerator() const
     {
         return numerator;
     }
-
     int get_denominator() const
     {
         return denominator;
     }
-
     void set_integer(int integer)
     {
         this->integer = integer;
     }
-
     void set_numerator(int numerator)
     {
         this->numerator = numerator;
     }
-
-    void set_denominator(int denominator)
+    void set_denominator(int demoinator)
     {
-        if (denominator = 0)
-            denominator = 1;
-        this->denominator = denominator;
+        if (demoinator = 0)
+            demoinator = 1;
+        this->denominator = demoinator;
     }
-
     //default constructor
     Fraction()
     {
@@ -50,7 +42,6 @@ public:
         this->numerator = int();
         cout << "Default constructor" << endl;
     }
-
     Fraction(int integer)
     {
         this->integer = integer;
@@ -66,15 +57,25 @@ public:
         set_denominator(denomantor);
     }
 
-    Fraction(int integer = 0, int numerator = 0, int denominator = 1)
+    Fraction(int integer, int numerator, int denominator)
     {
         this->integer = integer;
         this->denominator = denominator;
         this->numerator = numerator;
         cout << "Constructor" << endl;
     }
-
     ///Methods
+    void to_improrer()
+    {
+        numerator += integer * denominator;
+        integer = 0;
+    }
+
+    void to_proper()
+    {
+        integer += numerator / denominator;
+        numerator %= denominator;
+    }
 
     void print()
     {
@@ -98,9 +99,23 @@ public:
         cout << "Desctructor" << endl;
     }
 };
-//
+Fraction operator*(Fraction left, Fraction right)
+{
+    left.to_improrer();
+    right.to_improrer();
+    Fraction copy(
+        left.get_numerator() * right.get_numerator(),
+        left.get_denominator() * right.get_denominator()
+        );
+    // copy.set_numerator(left.get_numerator() * right.get_numerator());
+    // copy.set_denominator(left.get_denominator() * right.get_denominator());
+    // copy.to_proper();
+    return copy;
+}
+// #define CONSTUCTORCHECK
 int main()
 {
+#ifdef CONSTUCTORCHECK
     Fraction A;
     A.print();
 
@@ -110,8 +125,14 @@ int main()
     Fraction C(3, 4);
     C.print();
 
-    Fraction D(3, 4, 2);
+    Fraction D(2, 3, 4);
     D.print();
+#endif
+
+    Fraction A(2, 1, 2);
+    Fraction B(3, 2, 5);
+    Fraction C = A * B;
+    C.print();
 
     return 0;
 }
