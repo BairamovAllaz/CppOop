7a25639a304bfac74289a867ed431bf57558caae
Bayramov Allaz <67458149+bayramovellez@users.noreply.github.com>
2021-12-15 01:22:43 +0300

    HomeWork

--- DynamicMemoryClass/index.cpp
+++ DynamicMemoryClass/index.cpp
@@ -4,31 +4,100 @@ using namespace std;
 
 class String
 {
-    char *str = new char[80]{};
+    char *str;
+    int LengthOfString;
+
 public:
-    String(/* const char *Pstr */)
+    String(int size)
     {
-        // strcpy(str, Pstr);
+        this->LengthOfString = size;
+        str = new char[LengthOfString];
     }
+
+    String()
+    {
+        this->LengthOfString = 80;
+        this->str = new char[LengthOfString]{};
+    }
+    String(const char *n)
+    {
+        this->LengthOfString = strlen(n);
+        this->str = new char[LengthOfString]{};
+        strcpy(str, n);
+    }
+
+    char *getStr()
+    {
+        return this->str;
+    }
+
+    int getLength()
+    {
+        return this->LengthOfString;
+    }
+
     void setStr(const char *str1)
     {
+        this->LengthOfString = strlen(str1);
+        this->str = new char[LengthOfString]{};
         strcpy(str, str1);
     }
-    void print()
+    ostream &print(ostream &os) const
     {
-        cout << str << endl;
+        os << str << endl;
+        return os;
     }
+
+    istream &fill(istream &os)
+    {
+        os.getline(str, LengthOfString);
+        return os;
+    }
+
     ~String()
     {
-        delete[] str;
+        delete[] this->str;
     }
 };
+
+String operator+(String &left, String &right)
+{
+    int length = left.getLength() + right.getLength();
+    char retstring[length];
+    return String(strcat(left.getStr(), right.getStr()));
+}
+
+ostream &operator<<(ostream &os, const String &S)
+{
+    return S.print(os);
+}
+
+istream &operator>>(istream &is, String &S)
+{
+    return S.fill(is);
+}
+
 int main()
 {
-    // char arr[80];
-    // cin.getline(arr, 80);
     String s;
-    s.print();
+    cout << "Nothing: " << s << endl;
+    String str1 = "Hello";
+    String str2 = "World";
+    cout << "Str1: " << str1 << endl;
+    cout << "Str2: " << str2 << endl;
+    cout << "Your string: " << str1.getStr() << endl;
+
+    String str3 = str1 + str2;
+    cout << "Str3: " << str3 << endl;
+
+    String str4;
+    cout << "Enter str4: ";
+    cin >> str4;
+    cout << "Str4: " << str4 << endl;
+
+    String str5(25);
+    cout << "Nothing 2: " << endl;
+    cout << str5 << endl;
 
     return 0;
 }
