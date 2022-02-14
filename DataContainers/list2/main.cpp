8c4df4c5d692c7d18b84923b2ccece68849ede9d
Bairamov Allaz <ellezbayramov2004@gmail.com>
2022-02-14 14:26:50 +0300

    First

--- DataContainers/list2/main.cpp
+++ DataContainers/list2/main.cpp
@@ -54,6 +54,114 @@ public:
         size++;
     }
 
+    void push_back(int Data)
+    {
+        if (Head == nullptr && Tail == nullptr)
+        {
+            Head = Tail = new Element(Data);
+            size++;
+            return;
+        }
+        Element *new_element = new Element(Data);
+        new_element->pNext = nullptr;
+        new_element->pPrev = Tail;
+        Tail->pNext = new_element;
+        Tail = new_element;
+        size++;
+    }
+
+    void pop_front()
+    {
+        if (Head == nullptr && Tail == nullptr)
+        {
+            return;
+        }
+        Element *Erased = Head;
+        Head = Head->pNext;
+        Head->pPrev = nullptr;
+        delete Erased;
+        size--;
+    }
+
+    void pop_back()
+    {
+        if (Head == nullptr && Tail == nullptr)
+        {
+            return;
+        }
+        Element *Erased = Tail;
+        Tail = Tail->pPrev;
+        Tail->pNext = nullptr;
+        delete Erased;
+        size--;
+    }
+
+    void insert(int index, int data)
+    {
+        double newsize = size / 2;
+        Element *newElement = new Element(data);
+        Element *outTemp = nullptr;
+        if (index <= newsize || size < 5)
+        {
+            Element *Temp = Head;
+            for (int i = 0; i < index; ++i)
+            {
+                Temp = Temp->pNext;
+            }
+            outTemp = Temp;
+            size++;
+            cout << "Front" << endl;
+        }
+        else if (index >= newsize)
+        {
+            Element *Temp = Tail;
+            for (int i = 0; i < index; ++i)
+            {
+                Temp = Temp->pPrev;
+            }
+            outTemp = Temp;
+            size++;
+            cout << "Back" << endl;
+        }
+        newElement->pNext = outTemp;
+        newElement->pPrev = outTemp->pPrev;
+        outTemp->pPrev->pNext = newElement;
+        outTemp->pPrev = newElement;
+    }
+
+    void erase(int index)
+    {
+        double newsize = size / 2;
+        Element *outTemp = nullptr;
+        if (index <= newsize || size < 5)
+        {
+            Element *Temp = Head;
+            for (int i = 0; i < index; ++i)
+            {
+                Temp = Temp->pNext;
+            }
+            outTemp = Temp;
+            size--;
+            cout << "Front" << endl;
+        }
+        else if (index >= newsize)
+        {
+            Element *Temp = Tail;
+            for (int i = 0; i < index; ++i)
+            {
+                Temp = Temp->pPrev;
+            }
+            outTemp = Temp;
+            size--;
+            cout << "Back" << endl;
+        }
+
+        Element *Erased = outTemp;
+        outTemp->pPrev->pNext = outTemp->pNext;
+        outTemp->pNext->pPrev = outTemp->pPrev;
+        delete Erased;
+    }
+
     void print() const
     {
         Element *Temp = Head;
@@ -61,6 +169,7 @@ public:
         {
             cout << Temp->Data << endl;
         }
+        cout << "Size: " << size << endl;
     }
 
     void print_reverse() const
@@ -81,12 +190,24 @@ int main()
     List list;
     for (int i = 0; i < n; ++i)
     {
-        list.push_front(rand() % 20 + 1);
+        list.push_back(rand() % 20 + 1);
     }
-    cout << "Print " << endl;
-    list.print();
-    cout << "Reverse: " << endl;
+
+    cout << "Last: " << endl;
     list.print_reverse();
 
+    int index, number;
+    cout << "Enter index to add number: ";
+    cin >> index;
+    cout << "Enter number to add: ";
+    cin >> number;
+    list.insert(index, number);
+    cout << "Our: " << endl;
+    list.print_reverse();
+
+    cout << "Enter index to delete: ";
+    cin >> index;
+    list.erase(index);
+    list.print_reverse();
     return 0;
 }
