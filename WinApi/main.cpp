f3ef9d5abdfa44dd4db16322937545756e6c6fcf
BairamovAllaz <ellezbayramov2004@gmail.com>
2022-03-30 21:34:11 +0300

    win

--- WinApi/main.cpp
+++ WinApi/main.cpp
@@ -1,20 +1,82 @@
 #include <Windows.h>
 #include "resource.h"
+
+CHAR sz_login_invitation[] = "Enter login";
+CHAR sz_password_invitation[] = "Enter password";
+
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMgs, WPARAM wParam, LPARAM lParam);
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, INT nCmdShow) 
 {
 	//int Value_Message_Box = MessageBox(NULL, "HelloWINAPI", "INFO", MB_OKCANCEL | MB_ICONHAND);
-	DialogBoxParam(hInstance, MAKEINTRESOURCE(id_1), NULL, (DLGPROC)DlgProc, 0);
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(ID_CLOGIN), NULL, (DLGPROC)DlgProc, 0);
 	return 0;
 }
-
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-	switch (uMsg)
-	{
-	case WM_INITDIALOG:break;
-	case WM_COMMAND:break;
-	case WM_CLOSE:
-		EndDialog(hwnd, 0);
-	}
-	return FALSE;
+		switch (uMsg)
+		{
+		case WM_INITDIALOG: {
+			HICON hicon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+			SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hicon);
+			SendMessage(GetDlgItem(hwnd,IDC_LOGIN1), WM_SETTEXT, 0, (LPARAM)sz_login_invitation);
+			SendMessage(GetDlgItem(hwnd,IDC_LOGIN2), WM_SETTEXT, 0, (LPARAM)sz_password_invitation);
+		}
+			break;
+		case WM_COMMAND: {
+			switch (LOWORD(wParam))
+			{
+			case IDC_LOGIN1: {
+				switch (HIWORD(wParam))
+				{
+				case EN_SETFOCUS: {
+					SendMessage(GetDlgItem(hwnd, IDC_LOGIN1),WM_SETTEXT, 0, (LPARAM)"");
+				}
+				break;
+				default:
+					break;
+				}
+			}
+			break;
+			case IDC_LOGIN2: {
+				switch (HIWORD(wParam))
+				{
+				case EN_SETFOCUS: {
+					SendMessage(GetDlgItem(hwnd, IDC_LOGIN2), WM_SETTEXT, 0, (LPARAM)"");
+				} break;
+				default:
+					break;
+				}
+			}
+			break;
+			case IDC_COPYBUTTON: {
+				CONST INT SIZE = 256;
+				CHAR sz_buffer[SIZE] = {}; 
+				HWND hLogin = GetDlgItem(hwnd, IDC_LOGIN1); 
+				SendMessage(hLogin, WM_GETTEXT,SIZE, (LPARAM)sz_buffer);
+				HWND hPassword = GetDlgItem(hwnd, IDC_LOGIN2);
+				SendMessage(hPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+			}
+			break;
+			case IDC_SET_TITLE_BUTTON: {
+				CONST INT SIZE = 256;
+				CHAR sz_buffer[SIZE] = {};
+				SendMessage(GetDlgItem(hwnd, IDC_LOGIN1), WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+				SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_buffer); 
+			}
+			break;
+			case IDOK:{
+				MessageBox(NULL, "YOU PRESSED OK", "INFO", MB_OK | MB_ICONINFORMATION);
+			}
+				break;
+			case IDCANCEL: {
+				EndDialog(hwnd, 0);
+			}
+			default:
+				break;
+			}
+			break;
+		}
+		case WM_CLOSE:
+			EndDialog(hwnd, 0);
+		}
+		return FALSE;
 }
--- WinApi/resource.h
+++ WinApi/resource.h
@@ -2,17 +2,24 @@
 // Microsoft Visual C++ generated include file.
 // Used by Resource.rc
 //
-#define id_1                            9
+#define ID_CLOGIN                       3
 #define IDD_DIALOG1                     101
-#define IDD_DIALOG2                     103
+#define IDI_ICON1                       105
+#define IDC_LOGIN1                      1002
+#define IDC_LOGIN2                      1005
+#define IDC_STATICLOGIN                 1006
+#define IDC_STATICPASSWORD              1007
+#define IDC_COPYBUTTON                  1008
+#define IDC_BUTTON2                     1009
+#define IDC_SET_TITLE_BUTTON            1009
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_RESOURCE_VALUE        106
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1010
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
