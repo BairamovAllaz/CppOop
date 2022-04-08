f74e92fe47b341e95be2d9194ac23dc2f35d2ea2
BairamovAllaz <ellezbayramov2004@gmail.com>
2022-04-08 23:19:45 +0300

    winappi

--- MainWindow/Source.cpp
+++ MainWindow/Source.cpp
@@ -1,75 +1,91 @@
-#include <Windows.h>
-CONST CHAR g_szclassName[] = "My Window Class";
-LRESULT CALLBACK WndProc(HWND hwnd,UINT uMsg,WPARAM wparam,LPARAM lparam);
-INT WINAPI WINMAIN(HINSTANCE hinstance, HINSTANCE hPrevinstance, LPSTR lpCmdline,INT cCmdShow) 
+﻿#include<Windows.h>
+
+CONST CHAR g_szClassName[] = "My Window Class";
+//g  - global
+//sz - string zero
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
 {
-	WNDCLASSEX wc; //windows class
+	
+	WNDCLASSEX wc;	//wc - Window Class
 	ZeroMemory(&wc, sizeof(WNDCLASSEX));
 	wc.cbSize = sizeof(WNDCLASSEX);
-	wc.cbClsExtra = 0; 
-	wc.cbWndExtra = 0; 
-	wc.lpfnWndProc = WndProc;
-	wc.style = 0; 
-	wc.hIcon = LoadIcon(hinstance, MAKEINTRESOURCE(IDI_APPLICATION));
-	wc.hIconSm = LoadIcon(hinstance, MAKEINTRESOURCE(IDI_APPLICATION));
-	wc.hCursor = LoadCursor(hinstance, MAKEINTRESOURCE(IDC_ARROW)); 
-	wc.hbrBackground = (HBRUSH)COLOR_WINDOW; 
-	wc.hInstance = hinstance; 
-	wc.lpszMenuName = NULL; 
-	wc.lpszClassName = g_szclassName; 
-
-	if (!RegisterClassEx(&wc)) {
-		MessageBox(NULL,"Class registriation failed","Error",MB_OK | MB_ICONERROR);
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.lpfnWndProc = WndProc;	
+	wc.style = 0;	
+	wc.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));	
+	wc.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));	
+	wc.hCursor = LoadCursor(hInstance, MAKEINTRESOURCE(IDC_ARROW));
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.hInstance = hInstance;
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = g_szClassName;
+	if (!RegisterClassEx(&wc))
+	{
+		MessageBox(NULL, "Class registration failed", "Error", MB_OK | MB_ICONERROR);
 		return 0;
 	}
-	
-	//Creating window
 
-	HWND hwnd = CreateWindowEx(
+
+	int height_screen = GetSystemMetrics(SM_CYSCREEN); 
+	int width_screen = GetSystemMetrics(SM_CXSCREEN);
+
+
+	int window_width = width_screen / 2;  
+	int window_height = height_screen / 2; 
+
+	//2) Сохздание окна:
+	HWND hwnd = CreateWindowEx
+	(
+		NULL,	//Ex-style
+		g_szClassName,	
+		"Main Window",	
+		WS_OVERLAPPEDWINDOW,
+		(width_screen / 2)  - (window_width / 2),(height_screen / 2) - (window_height/ 2),
+		window_width, window_height,
+		NULL,	
 		NULL,
-		g_szclassName,
-		g_szclassName,
-		WS_EX_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT,
-		CW_USEDEFAULT, CW_USEDEFAULT,
-		NULL,
-		NULL,
-		hinstance,
+		hInstance,
 		NULL
 	);
-
-	if (hwnd == NULL) {
-		MessageBox(NULL, "WINDOW CREATION FAILED", "FAIL", MB_OK | MB_ICONERROR);
+	if (hwnd == NULL)
+	{
+		MessageBox(NULL, "Window creation failed", "Info", MB_OK | MB_ICONERROR);
 		return 0;
 	}
-	ShowWindow(hwnd, cCmdShow);
-	UpdateWindow(hwnd);
+	ShowWindow(hwnd, nCmdShow);	
+	UpdateWindow(hwnd);	
 
-	MSG msg; 
-
-	while (GetMessage(&msg, NULL, 0, 0) > 0) {
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0) > 0)
+	{
 		TranslateMessage(&msg);
-		DispatchMessage(&msg);
+		DispatchMessage(&msg);	
 	}
 
 	return msg.wParam;
 }
 
-
-LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wparam, LPARAM lparam) {
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
 	switch (uMsg)
 	{
-	case WM_CREATE: break; 
-	case WM_COMMAND: break; 
-	case WM_CLOSE: 
-		if (MessageBox(hwnd, "Are you sure close", "close", MB_YESNO | MB_ICONINFORMATION) == IDYES) {
+	case WM_CREATE:
+		break;
+	case WM_COMMAND:break;
+	case WM_CLOSE:
+		if (MessageBox(hwnd, "Вы действительно хотите закрыть окно?", "Шо, внатуре?", MB_YESNO | MB_ICONQUESTION) == IDYES)
+		{
 			DestroyWindow(hwnd);
 		}
 		break;
-	case WM_DESTROY : 
+	case WM_DESTROY:
 		PostQuitMessage(0);
-	default: {
-		return DefWindowProc(hwnd, uMsg, wparam, lparam);
+		break;
+	default:return DefWindowProc(hwnd, uMsg, wParam, lParam);
 	}
-	}
+	return 0;
 }
