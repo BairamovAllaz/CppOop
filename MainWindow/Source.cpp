976a274646a038b28627b6b99e24cf8b1277188d
BairamovAllaz <ellezbayramov2004@gmail.com>
2022-04-10 21:03:24 +0300

    winapi done

--- MainWindow/Source.cpp
+++ MainWindow/Source.cpp
@@ -1,7 +1,14 @@
-﻿#include<Windows.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <winapifamily.h>
+#include<Windows.h>
 #include<cstring>
 #include<string>
+#include <stdio.h>
+#include "resource.h"
 using namespace std;
+
+#define IDC_MY_STATIC 104
+
 
 CONST CHAR g_szClassName[] = "My Window Class";
 //g  - global
@@ -19,9 +26,11 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, in
 	wc.cbWndExtra = 0;
 	wc.lpfnWndProc = WndProc;	
 	wc.style = 0;	
-	wc.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));	
-	wc.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));	
-	wc.hCursor = LoadCursor(hInstance, MAKEINTRESOURCE(IDC_ARROW));
+	wc.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));	
+	wc.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));
+	//wc.hIcon = (HICON)LoadImage(hInstance,"new.ico",IMAGE_ICON,LR_DEFAULTSIZE,LR_DEFAULTSIZE,LR_LOADFROMFILE);
+	//wc.hIconSm = (HICON)LoadImage(hInstance, "new.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
+	wc.hCursor = LoadCursor(hInstance, MAKEINTRESOURCE(IDC_CURSOR1));
 	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
 	wc.hInstance = hInstance;
 	wc.lpszMenuName = NULL;
@@ -35,11 +44,10 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, in
 
 	int height_screen = GetSystemMetrics(SM_CYSCREEN); 
 	int width_screen = GetSystemMetrics(SM_CXSCREEN);
-
-
-
-	int window_width = width_screen / 2;  
-	int window_height = height_screen / 2; 
+	int window_width = width_screen - (width_screen / 4);  
+	int window_height = height_screen - (height_screen / 4);
+	int window_start_x = width_screen / 8;
+	int window_start_y = height_screen / 8;
 	//2) Сохздание окна:
 	HWND hwnd = CreateWindowEx
 	(
@@ -47,7 +55,8 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, in
 		g_szClassName,	
 		"Main Window",	
 		WS_OVERLAPPEDWINDOW,
-		(width_screen / 2)  - (window_width / 2),(height_screen / 2) - (window_height/ 2),
+		//(width_screen / 2)  - (window_width / 2),(height_screen / 2) - (window_height/ 2),
+		window_start_x,window_start_y,
 		window_width, window_height,
 		NULL,	
 		NULL,
@@ -61,10 +70,6 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, in
 	}
 	ShowWindow(hwnd, nCmdShow);	
 	UpdateWindow(hwnd);	
-
-
-
-
 
 	MSG msg;
 	while (GetMessage(&msg, NULL, 0, 0) > 0)
@@ -81,10 +86,36 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	switch (uMsg)
 	{
 	case WM_CREATE: {
-
+		CreateWindowEx(
+			NULL, 
+			"Static",
+			"this is my static text",
+			WS_CHILDWINDOW | WS_VISIBLE, 
+			10,10,
+			1000,50,
+			hwnd,
+			(HMENU)IDC_MY_STATIC,
+			GetModuleHandle(NULL), 
+			NULL
+		);
 	}
 	break;
 	case WM_COMMAND:break;
+	case WM_MOVE:
+	case WM_SIZE: {
+		CONST INT SIZE = 256; 
+		CHAR buffer[SIZE] = {}; 
+		RECT rect;
+		GetWindowRect(hwnd, &rect);
+		int window_width = rect.right - rect.left;
+		int window_height = rect.bottom - rect.top;
+		sprintf(buffer,"Size: %dx%d , Position: %dx%d",window_width,window_height, rect.left,rect.top);
+		SendMessage(hwnd,WM_SETTEXT,0,(LPARAM)buffer);
+		HWND hStatic = GetDlgItem(hwnd, IDC_MY_STATIC);
+		SendMessage(hStatic, WM_SETTEXT, 0, (LPARAM)buffer);
+
+	}
+	break;
 	case WM_CLOSE: {
 		/*int width, height;
 		RECT rect;
@@ -96,7 +127,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		std::string tmp = std::to_string(width);
 		char const* num_char = tmp.c_str();
 		*/
-		if (MessageBox(hwnd,"Are you sure to close?", "Шо, внатуре?", MB_YESNO | MB_ICONQUESTION) == IDYES)
+		if (MessageBox(hwnd,"Are you sure to close?", "Are You?", MB_YESNO | MB_ICONQUESTION) == IDYES)
 		{
 			DestroyWindow(hwnd);
 		}
